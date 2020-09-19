038263cda99b3e9fab5198cda5b6cfb1e21beaae
nbolt1989 <ncbolt89@gmail.com>
2020-09-19 14:39:11 -0400

    updates to the guage chart

--- static/startercode/plot.js
+++ static/startercode/plot.js
@@ -1,5 +1,5 @@
 //create a json path link
-const jsonPath= "/static/data/samples.json";
+const jsonPath = "/static/data/samples.json";
 
 //create a variable for the jsonData
 var jsonData;
@@ -12,13 +12,13 @@ d3.json(jsonPath).then((data) => {
 
 function init() {
     var myDropDown = d3.select('#selDataset');
-    jsonData.names.forEach((name)=>{
+    jsonData.names.forEach((name) => {
         myDropDown.append('option').text(name).property('value', name);
     });
     optionChanged(jsonData.names[0])
 };
 
-function optionChanged(personId){
+function optionChanged(personId) {
     //create barchart, bubblechart, demo stuff, and gauge chart
     barchart(personId);
     bubblechart(personId);
@@ -26,24 +26,24 @@ function optionChanged(personId){
     gaugechart(personId);
 };
 //create a function for the demographic stuff
-function demographicstuff(personId){
+function demographicstuff(personId) {
     var metaData = jsonData.metadata.filter((x) => x.id === parseInt(personId))[0];
     //console.log(metaData)
     var demoHTML = d3.select("#sample-metadata");
     demoHTML.html("");
-    Object.entries(metaData).forEach(([key, value]) => 
-        demoHTML.append("h4").text(`${key}: ${value}`));
+    Object.entries(metaData).forEach(([key, value]) =>
+        demoHTML.append("h6").text(`${key}: ${value}`));
 };
 
 // //create a function for the barChart
-function barchart(personId){
+function barchart(personId) {
     //create a variable for the sample data 
     var sampData = jsonData.samples.filter(obj => obj.id.toString() === personId)[0];
     //create a variable for the top ten OTUs
-    var topTenOtu = sampData.otu_ids.slice(0,13).reverse();
+    var topTenOtu = sampData.otu_ids.slice(0, 13).reverse();
     var otuIDs = topTenOtu.map(x => "OTU " + x);
-    var sampVals = sampData.sample_values.slice(0,10).reverse();
-    var sampLabels = sampData.otu_labels.slice(0,10).reverse();
+    var sampVals = sampData.sample_values.slice(0, 10).reverse();
+    var sampLabels = sampData.otu_labels.slice(0, 10).reverse();
     //build trace for bar chart
     var trace = {
         type: "bar",
@@ -65,10 +65,10 @@ function barchart(personId){
     //create my bar data variable for the trace
     var barData = [trace];
     //plot it
-    Plotly.newPlot('bar',barData,layout)
+    Plotly.newPlot('bar', barData, layout)
 };
 
-function bubblechart(personId){
+function bubblechart(personId) {
     //create a variable for the sample data 
     var sampData = jsonData.samples.filter(obj => obj.id.toString() === personId)[0];
     //create a variable for the sample data 
@@ -85,34 +85,49 @@ function bubblechart(personId){
             size: sampVals,
             colorscale: 'Hot'
         }
-      };
-      
-      var bubbledata = [trace1];
-      
-      var layout = {
+    };
+
+    var bubbledata = [trace1];
+
+    var layout = {
         title: 'Marker Size',
         showlegend: false,
         height: 600,
         width: 1200
-      };
-      
-      Plotly.newPlot('bubble', bubbledata, layout);
+    };
+
+    Plotly.newPlot('bubble', bubbledata, layout);
 
 };
 
-function gaugechart(personId){
+function gaugechart(personId) {
+    //create a metaData object like above 
     var metaData = jsonData.metadata.filter((x) => x.id === parseInt(personId))[0];
-    var washFreq = metaData.washFreq;
+    //console.log(metaData) -- doing this to make sure it prints out what I need
+    //now grab washing frequency from metaData
+    var washFreq = metaData.wfreq;
     var data = [
         {
             domain: { x: [0, 1], y: [0, 1] },
-            value: 270,
-            title: { text: "Washing Frequency" },
+            value: washFreq,
+            title: { text: "Belly Button Washing Frequency" },
             type: "indicator",
-            mode: "gauge+number"
-        }
+            mode: "gauge+number",
+            gauge: {
+                axis: {tickwidth: 1, tickcolor: "black" },
+                bar: { color: "darkgrey" },
+                bgcolor: "white",
+                borderwidth: 1,
+                bordercolor: "gray",
+                steps: [
+                    { range: [0, 1], color: "red" },
+                    { range: [1,3], color: "yellow"},
+                    { range: [3, 100], color: "green" }]
+                    },
+                }
     ];
-    
-    var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
+
+    var layout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
     Plotly.newPlot('gauge', data, layout);
 }
+
